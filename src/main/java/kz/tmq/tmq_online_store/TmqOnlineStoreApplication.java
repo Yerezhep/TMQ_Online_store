6d1770261b1e5b769b51b883f0cf7159d4e324b4
Sultan Yerezhep <yerezhepsultan11@gmail.com>
2022-06-13 11:58:03 +0600

    project start

---
+++ src/main/java/kz/tmq/tmq_online_store/TmqOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package kz.tmq.tmq_online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmqOnlineStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmqOnlineStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/tmq/tmq_online_store/TmqOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.tmq.tmq_online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmqOnlineStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
